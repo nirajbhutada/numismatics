22fc49fae6abc0522430db44072a79dcecbac72e
NIRAJ <nirajbhutada21@gmail.com>
2019-03-24 19:32:18 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,713 @@
+var notesData = [
+  {
+    "albumId": 1,
+    "id": 1,
+    "title": "786 notes",
+    "price":200,
+    "tags" : "786",
+    "imageName":"/786/786-1.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 2,
+    "title": "786 notes",
+    "price":300,
+    "tags" : "786",
+    "imageName":"/786/786-2.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 3,
+    "title": "786 notes",
+    "price":"750/bundle",
+    "tags" : "786",
+    "imageName":"/786/786-5-b.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 4,
+    "title": "786 notes",
+    "price":2700,
+    "tags" : "786",
+    "imageName":"/786/786-10-b.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 5,
+    "title": "786 notes",
+    "price":"30/note",
+    "tags" : "786",
+    "imageName":"/786/786-10-old.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 6,
+    "title": "786 notes",
+    "price":"50/note",
+    "tags" : "786",
+    "imageName":"/786/786-20.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 7,
+    "title": "786 notes",
+    "price":"999",
+    "tags" : "786",
+    "imageName":"786/786-20-0.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 8,
+    "title": "786 notes",
+    "price":"200/note",
+    "tags" : "786",
+    "imageName":"/786/786-50.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 9,
+    "title": "786 notes",
+    "price":"250",
+    "tags" : "786",
+    "imageName":"786/786-100.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 10,
+    "title": "786 notes",
+    "price":"300/note",
+    "tags" : "786",
+    "imageName":"/786/786-200.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 11,
+    "title": "786 notes",
+    "price":800,
+    "tags" : "786",
+    "imageName":"/786/786-200-2.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 12,
+    "title": "786 notes",
+    "price":"25/note",
+    "tags" : "786",
+    "imageName":"/786/786-10.jpeg"
+  },
+  {
+    "albumId": 1,
+    "id": 13,
+    "title": "786 notes",
+    "price":"60",
+    "tags" : "786",
+    "imageName":"/786/786-5-n.jpg"
+  },
+  {
+    "albumId": 1,
+    "id": 14,
+    "title": "786 notes",
+    "price":"60",
+    "tags" : "786",
+    "imageName":"/786/786-5-o.jpg"
+  },
+  {
+    "albumId": 1,
+    "id": 15,
+    "title": "786 notes",
+    "price":"50",
+    "tags" : "786",
+    "imageName":"/786/786-10.jpg"
+  },
+  {
+    "albumId": 1,
+    "id": 16,
+    "title": "786 notes",
+    "price":"800",
+    "tags" : "786",
+    "imageName":"/786/786-500-o.jpg"
+  },
+  {
+    "albumId": 1,
+    "id": 17,
+    "title": "786 notes",
+    "price":"250",
+    "tags" : "786",
+    "imageName":"/786/780006-100.jpg"
+  },
+  {
+    "albumId": 2,
+    "id": 18,
+    "title": "New Rs.1/- note with star in number",
+    "price":"50/note",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/star-1-n.jpg"
+  },
+  {
+    "albumId": 2,
+    "id": 19,
+    "title": "New Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/star-10-new.jpg"
+  },
+  {
+    "albumId": 2,
+    "id": 20,
+    "title": "New Rs.50/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/star-50-new.jpg"
+  },
+  {
+    "albumId": 2,
+    "id": 21,
+    "title": "New Rs.50/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/star-200-new.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 22,
+    "title": "Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_145403.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 23,
+    "title": "Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_145421.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 24,
+    "title": "Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_145452.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 25,
+    "title": "Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_145510.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 26,
+    "title": "Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_145635.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 27,
+    "title": "Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_145657.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 28,
+    "title": "Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_145842.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 29,
+    "title": "Rs.10/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_145917.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 30,
+    "title": "Rs.100/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_150241.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 31,
+    "title": "Rs.50/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_150443.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 32,
+    "title": "Rs.20/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_150558.jpg"
+  },
+   {
+    "albumId": 2,
+    "id": 33,
+    "title": "New Rs.20/- note with star in number",
+    "price":"Contact for Price",
+    "tags" : "star Notes",
+    "imageName":"/starnotes/IMG_20190324_150654.jpg"
+  },
+   {
+    "albumId": 3,
+    "id": 34,
+    "title": "Notes with date of birth",
+    "price":"Contact for Price",
+    "tags" : "Birthday Notes",
+    "imageName":"/birthdaynotes/070903-50.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 35,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-color-20.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 36,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-number-100.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 37,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-number-100-1.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 38,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-print-10.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 39,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-print-10-1.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 40,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-print-20.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 41,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-print-20-1.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 42,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-print-50.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 43,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-print-100.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 44,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-print-100-1.jpg"
+  },
+   {
+    "albumId": 4,
+    "id": 44,
+    "title": "Notes with mistake in number or printing",
+    "price":"Contact for Price",
+    "tags" : "Mistake Notes",
+    "imageName":"/mistakenotes/m-print-100-2.jpg"
+  },
+   {
+    "albumId": 5,
+    "id": 45,
+    "title": "Old Notes",
+    "price":"Contact for Price",
+    "tags" : "Old Notes",
+    "imageName":"/oldnotes/old-50.jpg"
+  },
+   {
+    "albumId": 5,
+    "id": 46,
+    "title": "Old Notes",
+    "price":"Contact for Price",
+    "tags" : "Old Notes",
+    "imageName":"/oldnotes/old-50-1.jpg"
+  },
+   {
+    "albumId": 6,
+    "id": 47,
+    "title": "Special Notes",
+    "price":"Contact for Price",
+    "tags" : "Special Notes",
+    "imageName":"/specialnotes/IMG_20190324_150931.jpg"
+  },
+   {
+    "albumId": 6,
+    "id": 48,
+    "title": "Special Notes",
+    "price":"Contact for Price",
+    "tags" : "Special Notes",
+    "imageName":"/specialnotes/special-print-10.jpg"
+  },
+   {
+    "albumId": 6,
+    "id": 49,
+    "title": "Special Notes",
+    "price":"Contact for Price",
+    "tags" : "Special Notes",
+    "imageName":"/specialnotes/special-seven-digit-100.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 50,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_143858.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 51,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_144012.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 52,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_144104.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 53,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_144218.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 54,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_144319.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 55,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_144436.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 56,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_144626.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 57,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_144633.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 58,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_144840.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 59,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_150116.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 60,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_150512.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 61,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_151244.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 62,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/IMG_20190324_151250.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 63,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-000001-100.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 64,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-000002-200.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 65,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-001000-2000.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 66,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-001001-10.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 67,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-005000-50.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 68,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-006003-5.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 69,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-010001-100.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 70,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-100000-1000.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 71,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-100001-50.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 72,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-112233-2000.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 73,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-242424-10.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 74,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-333888-5.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 75,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-390000-500.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 76,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-456567-500.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 77,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-500000-50.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 78,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-600000-100.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 79,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-755555-50.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 80,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-774433-50.jpg"
+  },
+   {
+    "albumId": 7,
+    "id": 81,
+    "title": "Notes with special numbers on it",
+    "price":"Contact for Price",
+    "tags" : "Special Number",
+    "imageName":"/specialnumber/sn-840085-100.jpg"
+  },
+   {
+    "albumId": 8,
+    "id": 82,
+    "title": "Notes without flagprinted on it",
+    "price":"Contact for Price",
+    "tags" : "Without Flag Notes",
+    "imageName":"/withoutflag/IMG_20190324_151050.jpg"
+  }
+]
+
+$( document ).ready(function() {
+    var targetedSection = $('#notes-section');
+    var mainContent = '';
+
+    //dynamic elements 
+    var imageName = '';
+    var imagePath = '';
+    var imagePrice = '';
+    var productCode = '';
+    var imagetitle = '';
+    var eachElem = '';
+    var initialEachElem = '<div class="mbr-gallery-item" data-tags="Jam" data-slide-to="0" data-seller="false" data-price="9"><div class="item_overlay" data-toggle="modal"></div><div class="galleryItem" data-toggle="modal"><div class="style_overlay"></div><div class="img_wraper">'+imagePath+'</div><span class="onsale mbr-fonts-style display-7" data-onsale="false" style="display: none;">-50%</span><div class="sidebar_wraper"><h4 class="item-title mbr-fonts-style mbr-text display-5">'+imagetitle+'</h4><div class="price-block"><span class="shop-item-price mbr-fonts-style display-5">Rs. '+imagePrice+'/-</span><div class="card-description">It is a nice item to buy.<br>Add some info here.<br><br>Product code: <strong>'+productCode+'</strong></div></div></div></div>';
+
+
+    
+    for(var i = 0; i < notesData.length;i++){
+    	imagePath = '<img src="/demo/assets/images/'+notesData[i].imageName+'" alt="" title="">';
+    	imagePrice = notesData[i].price;
+    	productCode = notesData[i].id;
+    	imagetitle = notesData[i].title;
+    	tags = notesData[i].tags;
+    	eachElem = '<div class="mbr-gallery-item" data-tags="'+tags+'" data-slide-to="'+i+'" data-seller="false" data-price="'+imagePrice+'"><div class="item_overlay" data-toggle="modal"></div><div class="galleryItem" data-toggle="modal"><div class="style_overlay"></div><div class="img_wraper">'+imagePath+'</div><span class="onsale mbr-fonts-style display-7" data-onsale="false" style="display: none;">-50%</span><div class="sidebar_wraper"><h4 class="item-title mbr-fonts-style mbr-text display-5">'+imagetitle+'</h4><div class="price-block"><span class="shop-item-price mbr-fonts-style display-5">Rs. '+imagePrice+'</span><div class="card-description">It is a nice item to buy.<br>Add some info here.<br><br>Product code: <strong>'+productCode+'</strong></div></div></div></div></div>'
+	    mainContent += eachElem;
+
+    }
+    $(targetedSection).append(mainContent);
+
+
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+      } else {
+        document.getElementById("myBtn").style.display = "none";
+      }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    $('#myBtn').on('click',function(){
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    })
+});
